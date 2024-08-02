67558be2cd58aa696f3d995adc1cda7a2048748d
Kaique Medeiro <kaique.medeiro14@hotmail.com>
2024-08-02 16:22:45 -0300

    colocando o texto da home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,17 @@
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam corrupti provident soluta quas tenetur, delectus voluptates! Corrupti sequi similique consequuntur animi nesciunt, distinctio unde! Quisquam nemo quos iste voluptatem nostrum!</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam corrupti provident soluta quas tenetur, delectus voluptates! Corrupti sequi similique consequuntur animi nesciunt, distinctio unde! Quisquam nemo quos iste voluptatem nostrum!</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam corrupti provident soluta quas tenetur, delectus voluptates! Corrupti sequi similique consequuntur animi nesciunt, distinctio unde! Quisquam nemo quos iste voluptatem nostrum!</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam corrupti provident soluta quas tenetur, delectus voluptates! Corrupti sequi similique consequuntur animi nesciunt, distinctio unde! Quisquam nemo quos iste voluptatem nostrum!</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam corrupti provident soluta quas tenetur, delectus voluptates! Corrupti sequi similique consequuntur animi nesciunt, distinctio unde! Quisquam nemo quos iste voluptatem nostrum!</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam corrupti provident soluta quas tenetur, delectus voluptates! Corrupti sequi similique consequuntur animi nesciunt, distinctio unde! Quisquam nemo quos iste voluptatem nostrum!</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam corrupti provident soluta quas tenetur, delectus voluptates! Corrupti sequi similique consequuntur animi nesciunt, distinctio unde! Quisquam nemo quos iste voluptatem nostrum!</p>
-
+      <h2>Quem somos?</h2>
+      <p>
+          A DC Representações nasceu após 8 anos de experiência do CEO fundador no ramo
+          de esquadrias de alumínio como projetista de conexões e vendas técnicas nas duas
+          principais indústrias do Brasil de conexões de esquadrias de alumínio, somando-se a experiências profissionais
+          anteriores,permitindo um vasto conhecimento acerca deste mercado em constante crescente e desenvolvimento.
+      </p>
+      <h2>Além da experiência com vendas técnicas, por que escolher a DC Representações?</h2>
+      <p>
+        Em um mercado com uma gama de produtos, você precisa de uma empresa de representações que entenda de venda direta, técnica e capaz de promover um bom marketing de acordo com o perfil e necessidade de cada produto, aumentando o seu alcance no mercado. Logo, a DC Representações é a empresa certa para isso. Acreditamos que a DC Representações tem o perfil para a sua empresa (ou nome da empresa). Caso tenham interesse, podemos agendar uma reunião de apresentação e esclarecimento de eventuais dúvidas. De antemão, agradeço o seu tempo até aqui. Em caso de dúvidas, estamos à disposição
+      </p>
     </div>
   );
 }
