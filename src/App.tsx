f666958a69f7aa3892a0914e2c22e3e0c66fa339
Kaique Medeiro <kaique.medeiro14@hotmail.com>
2024-08-02 14:08:40 -0300

    cirada pages e components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
-import { useState } from 'react'
+import Home from './pages/Home';
+import Clients from './pages/Clients';
 import './App.css'
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<Clients />} />
       </Routes>
     </>
   )
