f30363a120db1be15df16b0887169311e417f9fe
Kaique Medeiro <kaique.medeiro14@hotmail.com>
2024-08-02 16:13:38 -0300

    criado texto no footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,15 +8,19 @@ export default function Footer() {
       <h2>Como você pode nos encontrar?</h2>
       <div>
         <img src={ instagramLogo } alt="instagram logo" />
+        <p>representacoes.dc</p>
       </div>
       <div>
         <img src={ whatsAppLogo } alt="whatsApp logo" />
+        <p>xx-xxxxx-xxxx</p>
       </div>
       <div>
         <img src={ whatsAppLogo } alt="whatsApp logo" />
+        <p>xx-xxxxx-xxxx</p>
       </div>
       <div>
         <img src={ emailLogo } alt="email logo" />
+        <p>danilo@dc-representacoes.com</p>
       </div>
       <p>© 2024</p>
     </footer>
