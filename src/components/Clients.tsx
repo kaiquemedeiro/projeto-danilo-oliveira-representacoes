9baadd7a25ab59ec409d5d5e31711792d9e29c5b
Kaique Medeiro <kaique.mededeiro14@hotmail.com>
2024-08-07 17:30:29 -0300

    iniciando o carrosel de imagens

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,18 +1,12 @@
 import styles from './Clients.module.css';
 
+
 export default function Clients() {
   return (
     <div id="clients" className={ styles.Body }>
       <div className={ styles.titleContainer }>
         <h1>Clientes</h1>
       </div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nobis error, quos eum modi at? Tenetur cumque temporibus, porro perferendis ad quam corrupti inventore nobis laboriosam repudiandae totam tempora! Nostrum?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nobis error, quos eum modi at? Tenetur cumque temporibus, porro perferendis ad quam corrupti inventore nobis laboriosam repudiandae totam tempora! Nostrum?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nobis error, quos eum modi at? Tenetur cumque temporibus, porro perferendis ad quam corrupti inventore nobis laboriosam repudiandae totam tempora! Nostrum?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nobis error, quos eum modi at? Tenetur cumque temporibus, porro perferendis ad quam corrupti inventore nobis laboriosam repudiandae totam tempora! Nostrum?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nobis error, quos eum modi at? Tenetur cumque temporibus, porro perferendis ad quam corrupti inventore nobis laboriosam repudiandae totam tempora! Nostrum?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nobis error, quos eum modi at? Tenetur cumque temporibus, porro perferendis ad quam corrupti inventore nobis laboriosam repudiandae totam tempora! Nostrum?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nobis error, quos eum modi at? Tenetur cumque temporibus, porro perferendis ad quam corrupti inventore nobis laboriosam repudiandae totam tempora! Nostrum?</p>
     </div>
   );
 }
