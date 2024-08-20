811d58b53629035270fc93e34d5e62faec4af707
Kaique Medeiro <kaique.mededeiro14@hotmail.com>
2024-08-19 23:44:45 -0300

    link whatsApp, para botão flutuante

--- src/components/Whats.tsx
+++ src/components/Whats.tsx
@@ -4,7 +4,9 @@ import styles from './Whats.module.css';
 export default function Whats() {
   return (
     <div id="whats" className={ styles.Body}>
-      <img src={ whatsApp } alt="Pngtree—whatsapp mobile software icon_8704828.png " />
+      <a href="https://wa.me/11942674249?text=Tenho%20interesse%20em%20saber%20mais%20sobre%20a%20DC." target='_blank'>
+        <img src={ whatsApp } alt="Pngtree—whatsapp mobile software icon_8704828.png " />
+      </a>
     </div>
   );
 }
